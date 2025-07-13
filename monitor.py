5705b746eaa1effbeaa5f1cf10287368f4823e68
RT-Trading <Reccardotrading@gmail.com>
2025-07-13 16:34:09 +0200

    Create monitor.py

---
+++ monitor.py
@@ -0,0 +1,10 @@
+import time
+from main import check_trades, log_error
+
+if __name__ == "__main__":
+    while True:
+        try:
+            check_trades()
+        except Exception as e:
+            log_error(f"Hauptfehler (Worker): {e}")
+        time.sleep(600)  # alle 10 Minuten
